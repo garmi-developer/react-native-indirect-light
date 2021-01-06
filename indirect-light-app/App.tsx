488751af17f79afbc1b6eb15dd3420492d815618
各務佑 <kagamiyuu22@gmail.com>
2021-01-06 21:34:40 +0900

    背景変更、アロー関数に変更

--- indirect-light-app/App.tsx
+++ indirect-light-app/App.tsx
@@ -2,20 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>茶色にへんこう</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8b4513',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
