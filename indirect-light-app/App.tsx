6a9cf70fe6b59adc0098c8844c03ac97c3d8c520
各務佑 <kagamiyuu22@gmail.com>
2021-01-07 21:46:39 +0900

    カラー選択ライブラリ追加

--- indirect-light-app/App.tsx
+++ indirect-light-app/App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ColorPalette from 'react-native-color-palette';
 
 const App = () => {
   // TODO: 背景色をカラーピッカーから選択した値に変更する
   return (
     <View style={styles.container}>
+      <ColorPalette
+        onChange={(value) => alert(`Color selected: ${value}`)}
+        defaultColor={'#C0392B'}
+        colors={['#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9']}
+        title={'Uncontrolled Color Palette:'}
+      />
       <Text>茶色にへんこう</Text>
       <StatusBar style="auto" />
     </View>
