7380d63ddaafc63477f60013d176ee60fd8adfc1
各務佑 <kagamiyuu22@gmail.com>
2021-01-07 21:56:18 +0900

    背景色設定の処理を追加

--- indirect-light-app/App.tsx
+++ indirect-light-app/App.tsx
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ColorPalette from 'react-native-color-palette';
 
 const App = () => {
   // TODO: 背景色をカラーピッカーから選択した値に変更する
+  const [selectedColor, setSelectedColor] = useState('');
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: selectedColor }]}>
       <ColorPalette
-        onChange={(value) => alert(`Color selected: ${value}`)}
+        onChange={(value) => setSelectedColor(value)}
         defaultColor={'#C0392B'}
         colors={['#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9']}
-        title={'Uncontrolled Color Palette:'}
+        title={'背景色を変更する'}
       />
-      <Text>茶色にへんこう</Text>
       <StatusBar style="auto" />
     </View>
   );
